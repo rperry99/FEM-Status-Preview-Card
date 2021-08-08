33994116a4dfa1f949e5f6a6bf530e7e6ce81201
Russ <rperry1886@gmail.com>
2021-08-08 14:34:36 -0400

    Add mobile version of the card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import './App.css';
 
 function App() {
-  return null;
+  return (
+    <>
+      <div className="card">
+        <div className="image-container">
+          <img
+            src="./images/image-header-mobile.jpg"
+            alt="People in an open office workspace"
+          />
+          <div className="overlay"></div>
+        </div>
+
+        <div className="content-container">
+          <h1>
+            Get <span>insights</span> that help your business grow.
+          </h1>
+          <p className="description">
+            Discover the benefits of data analytics and make better decisions
+            regarding revanue, customer experience, and overall efficiency.
+          </p>
+          <div className="stats">
+            <div className="stat">
+              <p className="stat-count">10k+</p>
+              <p className="stat-title">Companies</p>
+            </div>
+            <div className="stat">
+              <p className="stat-count">314</p>
+              <p className="stat-title">Templates</p>
+            </div>
+            <div className="stat">
+              <p className="stat-count">12M+</p>
+              <p className="stat-title">Queries</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
